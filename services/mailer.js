ec4b1fce50c075a5deaf587083ebd3a5b806fa36
Amir Sibat <asibat@wiser.com>
2019-03-10 16:29:07 +0200

    Implement mailer functionality

---
+++ services/mailer.js
@@ -0,0 +1,48 @@
+const nodemailer = require('nodemailer')
+const nodemailerSendgrid = require('nodemailer-sendgrid')
+
+const { errorLog } = require('../utils/helpers')
+
+const options = {
+  apiKey: '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'
+}
+
+module.exports = class Mailer {
+  constructor() {
+    this.client = nodemailer.createTransport(nodemailerSendgrid(options))
+  }
+
+  async sendMail(emailAddress, data) {
+    let response
+    const { name, closingDetails, drawdownsDetails, maxDrawdown, stockReturn, dates } = data
+    const message = {
+      from: 'stock@manager.com',
+      to: emailAddress,
+      subject: 'Your EOD stock details',
+      dynamic_template_data: {
+        name,
+        closingDetails,
+        drawdownsDetails,
+        maxDrawdown,
+        stockReturn,
+        dates
+      },
+      template_id: 'd-3d7d201952054a43acd84e75e4e83402'
+    }
+
+    try {
+      response = await this.client.sendMail(message)
+    } catch (err) {
+      console.log('Errors occurred, failed to deliver message')
+
+      if (err.response && err.response.body && err.response.body.errors) {
+        err.response.body.errors.forEach(error => errorLog([error.field, error.message]))
+      } else {
+        console.log(err)
+      }
+      return false
+    }
+    console.log('Message delivered with code %s %s', response[0].statusCode, response[0].statusMessage)
+    return true
+  }
+}
